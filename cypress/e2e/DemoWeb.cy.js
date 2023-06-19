e9eb90b3047363ac29ff84a150adef733281e9fa
Rafael Suzano <rafaelsuzano@hotmail.com>
2023-06-19 13:37:02 +0100

    report

--- cypress/e2e/DemoWeb.cy.js
+++ cypress/e2e/DemoWeb.cy.js
@@ -1,5 +1,5 @@
 describe('Demo Cypress', () => {
-  it('passes', () => {
+  it('Fluxo Principal', () => {
     cy.visit('https://form.jotform.com/231692676005660')
     cy.get('input[id="first_20"]').type('rafael')
     cy.get('input[id="last_20"]').type('Suzano')
@@ -22,6 +22,72 @@ describe('Demo Cypress', () => {
     
 
 })
+
+it('Campos Obrigatórios Email', () => {
+  cy.visit('https://form.jotform.com/231692676005660')
+  cy.get('input[id="first_20"]').type('rafael')
+  cy.get('input[id="last_20"]').type('Suzano')
+  cy.get('input[id="input_19_full"]').type('(111) 111-1111')
+  //cy.get('input[id="input_23"]').type('rafaelsuzano@hotmail.com')
+
+
+  cy.get('input[id="input_21_addr_line1"]').type('a')
+  cy.get('input[id="input_21_addr_line2"]').type('a')
+  cy.get('input[id="input_21_city"]').type('a')
+  cy.get('input[id="input_21_state"]').type('a')
+  cy.get('input[id="input_21_postal"]').type('111111111')
+
+  cy.get('#input_2').click();
+
+  
+  cy.get('*[class^="form-line jf-required form-line-error"]').
+  should('be.visible')
+
+  cy.contains('This field is require')
+
+
+  
+  cy.get('*[class^="error-navigation-message"]').
+  should('be.visible')
+
+  cy.contains('This field is require')
+  
+
+})
+
+
+it('Campos Obrigatórios nome', () => {
+  cy.visit('https://form.jotform.com/231692676005660')
+ // cy.get('input[id="first_20"]').type('rafael')
+  cy.get('input[id="last_20"]').type('Suzano')
+  cy.get('input[id="input_19_full"]').type('(111) 111-1111')
+  cy.get('input[id="input_23"]').type('rafaelsuzano@hotmail.com')
+
+
+  cy.get('input[id="input_21_addr_line1"]').type('a')
+  cy.get('input[id="input_21_addr_line2"]').type('a')
+  cy.get('input[id="input_21_city"]').type('a')
+  cy.get('input[id="input_21_state"]').type('a')
+  cy.get('input[id="input_21_postal"]').type('111111111')
+
+  cy.get('#input_2').click();
+
+  
+  cy.get('*[class^="form-line jf-required form-line-error"]').
+  should('be.visible')
+
+  cy.contains('This field is require')
+
+
+  
+
+  
+
+})
+
+
+
+
 })
 
 
