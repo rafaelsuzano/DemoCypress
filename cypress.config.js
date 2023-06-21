919550aa4592d34da905d970d86203a840fec15a
Rafael Suzano <rafaelsuzano@hotmail.com>
2023-06-21 12:16:51 +0100

    novos ajustes

--- cypress.config.js
+++ cypress.config.js
@@ -12,18 +12,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
-     on('before:browser:launch', (browser = {}, launchOptions) => {
-                console.log(launchOptions.args)
 
-                if (browser.family == 'chromium') {
-                    launchOptions.args.push('--disable-gpu')
-                }
-
-                return launchOptions
-            });
-
-
-      
     },
   },
 });
