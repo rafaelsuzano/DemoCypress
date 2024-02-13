5b229720a327d082953c9f34d23b556a63acfb4d
Rafael Suzano <rafaelsuzano@hotmail.com>
2024-02-13 11:30:59 -0300

    ajustes projeto

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
     charts: true,
-    reportPageTitle: 'custom-title',
+    reportPageTitle: 'Demo Cypress Web/API',
     embeddedScreenshots: true,
     inlineAssets: true,
     saveAllAttempts: false,
