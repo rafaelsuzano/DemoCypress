b10201c0b8766d48f40cd2dc0a7a147cf2a3a9a4
Rafael Suzano <rafaelsuzano@hotmail.com>
2025-03-31 16:13:38 -0300

    ajuste na geracao do relatorio

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,32 @@
 const { defineConfig } = require('cypress');
 
+
+
 module.exports = defineConfig({
-  reporter: 'mochawesome',
-  reporterOptions: {
-    reportDir: 'cypress/reports',
-    overwrite: false,
-    html: false,
-    json: true
-  },
+
+  async setupNodeEvents(on, config) {
+
+},
+
   e2e: {
-
-
+    retries:2,
+    defaultCommandTimeout: 60000, // Set to 60 seconds
+    experimentallSudio: true,
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
   },
-);
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports', // Diretório dos relatórios
+    overwrite: true,            // Não sobrescreve relatórios anteriores
+    html: true,                 // Apenas JSON
+    json: false      ,             // Necessário para combinar os relatórios
+    embeddedScreenshots: true,
+    inlineAssets: true,  // Inclui capturas de tela diretamente no relatório
+    reportTitle: "Relatório Testes ",
+    reportPageTitle: "Relatório Testes ",
+
+  },
+});
