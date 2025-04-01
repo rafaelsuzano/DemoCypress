88ffea35cf01908ae7b52fd51d40d4864bcb769b
Rafael Suzano <rafaelsuzano@hotmail.com>
2025-03-31 23:13:37 -0300

    conceito Application Actions

--- cypress/e2e/FE/DemoWeb.cy.js
+++ cypress/e2e/FE/DemoWeb.cy.js
@@ -1,89 +1,28 @@
+import { mensagem, FluxoCadastro } from "../../support/utils"
+
 describe('Demo Cypress', () => {
-    beforeEach(() => {
+  beforeEach(() => {
 
-       // cy.viewport(1280, 1080)
+  })
+
+
+  it('Fluxo Cadastro', () => {
+    cy.fixture('cadastro.json').then((dados) => {
+      dados.forEach((dados) => {
+        cy.log(dados.cenario)
+        cy.visit('https://form.jotform.com/231692676005660')
+
+        FluxoCadastro(String(dados.FirstName), String(dados.LastName), String(dados.cep), String(dados.email), String(dados.esperado), dados.telefone)
+
+
+        cy.screenshot(dados.cenario)
+        cy.addTestContext(dados.cenario);
+        cy.reload()
+
       })
-
-
-  it('Fluxo Principal', () => {
-    cy.visit('https://form.jotform.com/231692676005660')
-    cy.get('input[id="first_20"]').type('rafael')
-    cy.get('input[id="last_20"]').type('Suzano')
-    cy.get('input[id="input_19_full"]').type('(111) 111-1111')
-    cy.get('input[id="input_23"]').type('rafaelsuzano@hotmail.com')
-
-
-    cy.get('input[id="input_21_addr_line1"]').type('a')
-    cy.get('input[id="input_21_addr_line2"]').type('a')
-    cy.get('input[id="input_21_city"]').type('a')
-    cy.get('input[id="input_21_state"]').type('a')
-    cy.get('input[id="input_21_postal"]').type('111111111')
-
-    cy.get('#input_2').click();
-
-    cy.get('*[class^="thankyou-sub-text ty-text"]').
-    should('be.visible')
-
-    cy.contains('Thank You!')
-    
-    cy.addTestContext('Mensagem de Teste');
-    cy.screenshot()
-
+    })
+  })
 })
 
-it('Campo Obrigatório Email', () => {
-  cy.visit('https://form.jotform.com/231692676005660')
-  cy.get('input[id="first_20"]').type('rafael')
-  cy.get('input[id="last_20"]').type('Suzano')
-  cy.get('input[id="input_19_full"]').type('(111) 111-1111')
-  //cy.get('input[id="input_23"]').type('rafaelsuzano@hotmail.com')
-
-
-  cy.get('input[id="input_21_addr_line1"]').type('a')
-  cy.get('input[id="input_21_addr_line2"]').type('a')
-  cy.get('input[id="input_21_city"]').type('a')
-  cy.get('input[id="input_21_state"]').type('a')
-  cy.get('input[id="input_21_postal"]').type('111111111')
-  cy.get('#input_2').click();
-
-
-  cy.get('*[class^="form-line jf-required form-line-error"]').
-  should('be.visible')
-
-  cy.contains('This field is require')
-  cy.get('*[class^="error-navigation-message"]').
-  should('be.visible')
-  cy.contains('This field is require')
-  cy.screenshot()
-
-})
-
-
-it('Campo Obrigatório nome', () => {
-  cy.visit('https://form.jotform.com/231692676005660')
- // cy.get('input[id="first_20"]').type('rafael')
-  cy.get('input[id="last_20"]').type('Suzano')
-  cy.get('input[id="input_19_full"]').type('(111) 111-1111')
-  cy.get('input[id="input_23"]').type('rafaelsuzano@hotmail.com')
-  cy.get('input[id="input_21_addr_line1"]').type('a')
-  cy.get('input[id="input_21_addr_line2"]').type('a')
-  cy.get('input[id="input_21_city"]').type('a')
-  cy.get('input[id="input_21_state"]').type('a')
-  cy.get('input[id="input_21_postal"]').type('111111111')
-
-  cy.get('#input_2').click();
-
-
-  cy.get('*[class^="form-line jf-required form-line-error"]').
-  should('be.visible')
-
-  cy.contains('This field is require')
-  cy.screenshot()
-
-
-})
-
-
-})
 
 
--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -1,30 +1,23 @@
-function mensagem(texto) {
-cy.log("ola")
+export function mensagem(texto) {
+cy.log("ola" + texto)
 
 }
 
-function cadastro (nome,sobrenome)
+
+export function FluxoCadastro(FirstName,LastName,cep,email,esperado,telefone)
 {
-    cy.get('input[id="first_20"]').type(nome)
-    cy.get('input[id="last_20"]').type(sobrenome)
-    cy.get('input[id="input_19_full"]').type('(111) 111-1111')
-    cy.get('input[id="input_23"]').type('rafaelsuzano@hotmail.com')
+
+   cy.get('input[id="first_20"]').type(FirstName)
+   cy.get('input[id="last_20"]').type(LastName)
+   cy.get('input[id="input_19_full"]').type(telefone)
+   cy.get('input[id="input_23"]').type(email)
+   cy.get('input[id="input_21_addr_line1"]').type('a')
+   cy.get('input[id="input_21_addr_line2"]').type('a')
+   cy.get('input[id="input_21_city"]').type('a')
+   cy.get('input[id="input_21_state"]').type('a')
+   cy.get('input[id="input_21_postal"]').type(cep)
+   cy.get('#input_2').click();
 
 
-    cy.get('input[id="input_21_addr_line1"]').type('a')
-    cy.get('input[id="input_21_addr_line2"]').type('a')
-    cy.get('input[id="input_21_city"]').type('a')
-    cy.get('input[id="input_21_state"]').type('a')
-    cy.get('input[id="input_21_postal"]').type('111111111')
-
-    cy.get('#input_2').click();
-
-    cy.get('*[class^="thankyou-sub-text ty-text"]').
-    should('be.visible')
-
-    cy.contains('Thank You!')
+   cy.contains(esperado)
 }
- module.exports = {
-    cadastro,
-    mensagem
- }
