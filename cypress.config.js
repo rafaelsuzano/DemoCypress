9ce399296f8980b08e2f87250899a94ac4d0b419
Rafael Suzano Cruz <rafaelsuzano@MacBook-Air-de-Rafael.local>
2025-07-24 00:20:31 -0300

    ajuste

--- cypress.config.js
+++ cypress.config.js
@@ -1,32 +1,19 @@
 const { defineConfig } = require('cypress');
 
-
-
 module.exports = defineConfig({
-
-  async setupNodeEvents(on, config) {
-
-},
-
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Demo Cypress Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    reportDir: 'cypress/reports',
+  },
   e2e: {
-    retries:2,
-    defaultCommandTimeout: 60000, // Set to 60 seconds
-    experimentallSudio: true,
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
       return config;
-    },
-  },
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    reportDir: 'cypress/reports', // Diretório dos relatórios
-    overwrite: true,            // Não sobrescreve relatórios anteriores
-    html: true,                 // Apenas JSON
-    json: false      ,             // Necessário para combinar os relatórios
-    embeddedScreenshots: true,
-    inlineAssets: true,  // Inclui capturas de tela diretamente no relatório
-    reportTitle: "Relatório Testes ",
-    reportPageTitle: "Relatório Testes ",
-
-  },
+    }
+  }
 });
